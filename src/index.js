1c2e10b219d8a16e4346c278b670d848fb27670f
lidasharova <Lida050496@yandex.ru>
2023-01-08 20:48:16 +0700

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+	str = ""+ n ;
+	if (str[0]=== '-'){
+		let b = str.slice(1, str.length);
+		return Number([...b].reverse().join(""));
+	}
+  return Number([...str].reverse().join(''))
 }
